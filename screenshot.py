165ff1d94630c7c3413e812b05722b97641497a4
Joshua Auriemma <allocate@gmail.com>
2023-01-11 17:06:31 -0500

    Improvements

--- screenshot.py
+++ screenshot.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import cv2
 import time
+import win32api
 from PIL import ImageGrab
 from PIL import Image
 from pywinauto import Desktop
@@ -13,26 +14,27 @@ cheat_path = 'C:\\Users\\allocate\\development\\cookies\\cheat.jpg'
 TESTING = False
 AUTOCLICK = True
 
+
 def get_cookie_clicker_screenshot():
     if TESTING:
         img = Image.open(cheat_path).convert('L')
         img = np.array(img)
-        ret, img = cv2.threshold(
-            img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
+        ret, img = cv2.threshold(img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
         return img
     title_pattern = re.compile(r"^.*cookies.*Cookie Clicker.*")
     all_windows = Desktop().windows()
     for window in all_windows:
         if title_pattern.match(window.texts()[0]):
-            window.set_focus()
+            current_x, current_y = win32api.GetCursorPos()
             x, y, width, height = window.rectangle().left, window.rectangle(
             ).top, window.rectangle().width(), window.rectangle().height()
+            mouse.move(coords=(x, y))
             img = ImageGrab.grab(
                 bbox=(x, y, x + width, y + height)).convert('L')
-            #window.minimize()
             img = np.array(img)
             ret, img = cv2.threshold(
                 img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
+            mouse.move(coords=(current_x, current_y))
             return img
     else:
         print("Cookie Clicker window not found")
@@ -58,7 +60,8 @@ def find_golden_cookie(screenshot, kp_reference, des_reference, img_reference):
     sift = cv2.SIFT_create()
     # Finding keypoints and descriptor of the screenshot
     game_screenshot = screenshot
-    kp_screenshot, des_screenshot = sift.detectAndCompute(game_screenshot, None)
+    kp_screenshot, des_screenshot = sift.detectAndCompute(
+        game_screenshot, None)
     # create BFMatcher object
     bf = cv2.BFMatcher(cv2.NORM_L2)
     # Match descriptors.
@@ -69,28 +72,30 @@ def find_golden_cookie(screenshot, kp_reference, des_reference, img_reference):
     for match in matches:
         if len(match) == 2 and match[0].distance < match[1].distance * ratio:
             good_matches.append(match[0])
-    img_matches = cv2.drawMatches(img_reference, kp_reference, screenshot, kp_screenshot, good_matches, None)
+    img_matches = cv2.drawMatches(
+        img_reference, kp_reference, screenshot, kp_screenshot, good_matches, None)
     # Obtain the coordinates of the best match
     if len(good_matches) > 0:
         x, y = kp_screenshot[good_matches[0].trainIdx].pt
-        #print("Match found")
+        # print("Match found")
         return x, y
     else:
-        #print("No match found")
+        # print("No match found")
         return None
 
 
+current_x, current_y = win32api.GetCursorPos()
+
 while True:
     screenshot = get_cookie_clicker_screenshot()
-    kp_reference, des_reference, img_reference = prepare_reference_image(reference_path)
-    coordinates = find_golden_cookie(screenshot, kp_reference, des_reference, img_reference)
-    if coordinates is not None:
-        x, y = coordinates
-        print("Golden cookie found at coordinates:", x, y)
-        if AUTOCLICK:
-            mouse.click(button='left', coords=(int(x), int(y)))
-            
-            
-    #else:
-    #    print("No golden cookie found")
+    kp_reference, des_reference, img_reference = prepare_reference_image(
+        reference_path)
+    coords = find_golden_cookie(screenshot, kp_reference, des_reference, img_reference)
+    if coords and AUTOCLICK:
+        x, y = coords
+        # move the cursor to the golden cookie and click
+        mouse.move(coords=(x, y))
+        mouse.click(button='left')
+        # move cursor back to its initial position
+        mouse.move(coords=(current_x, current_y))
     time.sleep(1)
